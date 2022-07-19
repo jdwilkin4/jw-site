18b311e034d7b157fd52fb04a397a053c5423ff6
jdwilkin4 <jwilkin4@hotmail.com>
2022-07-18 22:42:20 -0700

    chore: adding tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="text-5xl font-bold text-sky-500	">testing</p>
       </header>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.tsx",
+    "./public/**/*.html",
+    "./public/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
