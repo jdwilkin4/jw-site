0f76a4155490561906487027ceaf8f285cbf37fb
jdwilkin4 <jwilkin4@hotmail.com>
2022-07-19 22:48:49 -0700

    feat: adding design for navbar

---
+++ src/components/MobileMenuIcon.tsx
@@ -0,0 +1,37 @@
+export default function MobileMenuIcon() {
+  return (
+    <>
+      <svg
+        className="block h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+
+      <svg
+        className="hidden h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,74 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import MobileMenuIcon from "./MobileMenuIcon";
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const menuOptions: string[] = [
+    "Home",
+    "About",
+    "Articles",
+    "Media",
+    "Contact",
+  ];
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/articles">Articles</Link>
-      <Link to="/media">Media</Link>
-      <Link to="/contact">Contact</Link>
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <MobileMenuIcon />
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <NavLinks options={menuOptions} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sm:hidden" id="mobile-menu">
+        {isOpen && (
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            <NavLinks options={menuOptions} block="block" />
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
+interface Props {
+  options: string[];
+  block?: string;
+}
+
+const NavLinks: React.FC<Props> = ({ options, block }: Props) => {
+  return (
+    <>
+      {options.map((subPage) => (
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? `bg-gray-900 text-white px-3  py-2 rounded-md text-sm font-medium ${block}`
+              : `text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${block}`
+          }
+          to={`/${subPage === "Home" ? "" : subPage.toLowerCase()}`}
+        >
+          {subPage}
+        </NavLink>
+      ))}
+    </>
+  );
+};
