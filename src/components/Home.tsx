7735edf616a7f11c7e163765e4cf373749ca16e0
jdwilkin4 <jwilkin4@hotmail.com>
2022-07-26 23:24:21 -0700

    feat: creating hero section for home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <h1>Home </h1>;
+  return (
+    <>
+      <div className="text-center mt-10">
+        <h1 className="text-6xl my-6">Jessica Wilkins</h1>
+        <h2 className="text-5xl">Software Engineer | Technical Writer</h2>
+        <ul className="list-none text-3xl mt-20">
+          <li className="my-4">I love to learn new technologies</li>
+          <li className="my-4">I love to help people get into tech</li>
+          <li className="my-4">
+            I love to spread black girl magic around the world
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
