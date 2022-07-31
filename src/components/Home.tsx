f68f86c8157d97cfb1be913ac48a329376c60b74
jdwilkin4 <jwilkin4@hotmail.com>
2022-07-31 00:46:53 -0700

    fix: fix hero section text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,10 +6,10 @@ export default function Home() {
         <h2 className="text-5xl">Software Engineer | Technical Writer</h2>
         <ul className="list-none text-3xl mt-20">
           <li className="my-4">I love to learn new technologies</li>
-          <li className="my-4">I love to help people get into tech</li>
           <li className="my-4">
-            I love to spread black girl magic around the world
+            I love to write about my experiences and knowledge
           </li>
+          <li className="my-4">I love to help people get into tech</li>
         </ul>
       </div>
     </>
