7c29e2dca967d12eac30e6e5fc90fab009f20c96
Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>
2022-08-01 22:10:34 -0700

    feat: adding projects section (#20)
    * feat: adding projects section
    
    * feat: adding homepage image

--- src/exports.ts
+++ src/exports.ts
@@ -1,5 +1,6 @@
 export { default as Hero } from "./components/HeroSection";
 export { default as About } from "./components/About";
+export { default as Projects } from "./components/Projects";
 export { default as Articles } from "./components/Articles";
 export { default as Contact } from "./components/Contact";
 export { default as Media } from "./components/Media";
