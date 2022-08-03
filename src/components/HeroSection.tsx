c6cc61fc44fc362f1abd5a3bf05d4bd2651d6b2c
jdwilkin4 <jwilkin4@hotmail.com>
2022-08-03 05:56:29 -0700

    chore: cleaning up hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
       <h2 className="text-5xl">Software Engineer | Technical Writer</h2>
       <ul className="list-none text-3xl mt-20">
         <li className="my-4">I love to learn new technologies</li>
-        <li className="my-4">I love to write about my experiences</li>
+        <li className="my-4">I love to write about tech</li>
         <li className="my-4">I love to help people get into tech</li>
       </ul>
     </div>
