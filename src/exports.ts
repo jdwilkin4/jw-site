8ba405d67df4fa323c0794958ee2dc8ae5c20c6e
jdwilkin4 <jwilkin4@hotmail.com>
2022-08-16 18:25:27 -0700

    feat: adding hero stories

--- src/exports.ts
+++ src/exports.ts
@@ -1,4 +1,4 @@
-export { default as Hero } from "./components/HeroSection";
+export { Hero } from "./components/hero/HeroSection";
 export { Header } from "./components/header/Header";
 export { About } from "./components/about/About";
 export { Contact } from "./components/contact/Contact";
