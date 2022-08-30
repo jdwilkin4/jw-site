a8a80fd294dc4bfc70c523874db9db5127ef68e5
Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>
2022-08-29 19:21:07 -0700

    chore: adding ally addon (#33)
    * chore: adding ally addon
    
    * fix: accessiblity errors

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ const path = require("path");
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
+    "@storybook/addon-a11y",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "storybook-addon-mock/register",
--- src/components/error-message/ErrorMessage.tsx
+++ src/components/error-message/ErrorMessage.tsx
@@ -1,7 +1,7 @@
 export const ErrorMessage: React.VFC = () => {
   return (
     <div role="alert">
-      <h2 className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+      <h2 className="bg-red-700 text-white font-bold rounded-t px-4 py-2">
         Error!
       </h2>
       <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
